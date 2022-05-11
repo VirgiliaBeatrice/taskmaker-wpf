23363f88524899dab379908c4d16185980bcd0d9
Haoyan.Li <haoyan.li.ac@gmail.com>
2022-05-11 18:41:58 +0900

    feat: BindableMotor for ObservationCollection

--- ViewModels/RegionMotorViewModel.cs
+++ ViewModels/RegionMotorViewModel.cs
@@ -5,21 +5,105 @@ using Prism.Services.Dialogs;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
 using System.Windows.Input;
 using taskmaker_wpf.Model.Data;
 using taskmaker_wpf.Services;
 
 namespace taskmaker_wpf.ViewModels {
-    public class RegionMotorViewModel : BindableBase, INavigationAware {
-        private ICommand removeMotorCmd;
-        public ICommand RemoveMotorCmd => removeMotorCmd ?? (removeMotorCmd = new DelegateCommand<Motor>(RemoveMotorCmdExecute));
+    public class BindableMotor : BindableBase {
+        private int _motorId;
+        public int MotorId {
+            get => _motorId;
+            set {
+                SetProperty(ref _motorId, value);
+                Instance.MotorId = _motorId;            
+            } 
+        }
 
-        private void RemoveMotorCmdExecute(Motor obj) {
-            _motorService.Motors.Remove(obj);
-            Motors.Remove(obj);
+        private int _boardId;
+        public int BoardId {
+            get => _boardId;
+            set {
+                SetProperty(ref _boardId, value);
+                Instance.BoardId = _boardId;
+            } 
+        }
+
+        private string _name;
+        public string Name {
+            get => _name;
+            set {
+                SetProperty(ref _name, value);
+                Instance.Alias = _name;
+            }
+        }
+
+        private int _value;
+        public int Value {
+            get => _value;
+            set {
+                SetProperty(ref _value, value);
+                Instance.Value = _value;
+            }
+        }
+
+        private int _min;
+        public int Min {
+            get => _min;
+            set {
+                SetProperty(ref _min, value);
+                Instance.Min = _min;
+            }
+        }
+
+        private int _max;
+        public int Max {
+            get => _max;
+            set {
+                SetProperty(ref _max, value);
+                Instance.Max = _max;
+            }
+        }
+
+        public Motor Instance { get => _instance; }
+
+        private Motor _instance;
+
+        public BindableMotor(Motor motor) {
+            _motorId = motor.MotorId;
+            _boardId = motor.BoardId;
+            _name = motor.Alias;
+            _value = motor.Value;
+            _min = motor.Min;
+            _max = motor.Max;
+
+            _instance = motor;
+        }
+    }
+
+
+
+    public class RegionMotorViewModel : BindableBase, INavigationAware {
+        private ICommand connectMotorCmd;
+        public ICommand ConnectMotorCmd => connectMotorCmd ?? (connectMotorCmd = new DelegateCommand<BindableMotor>(ConnectMotorCmdExecute));
+
+        private void ConnectMotorCmdExecute(BindableMotor motor) {
+            var instance = _serialSrv.GetMotorInstance(motor.BoardId, motor.MotorId);
+
+            motor.Instance.Link(instance, motor.BoardId, motor.MotorId);
+        }
+
+        private ICommand removeMotorCmd;
+        public ICommand RemoveMotorCmd => removeMotorCmd ?? (removeMotorCmd = new DelegateCommand<BindableMotor>(RemoveMotorCmdExecute));
+
+        private void RemoveMotorCmdExecute(BindableMotor bMotor) {
+            Motors.Remove(bMotor);
+            _motorService.Motors.Remove(bMotor.Instance);
         }
 
         private ICommand addMotorCmd;
@@ -27,8 +111,10 @@ namespace taskmaker_wpf.ViewModels {
 
         private void AddMotorCmdExecute() {
             var newMotor = new Motor();
+            var newBindableMotor = new BindableMotor(newMotor);
+
             _motorService.Motors.Add(newMotor);
-            Motors.Add(newMotor);
+            Motors.Add(newBindableMotor);
         }
 
         private ICommand setCmd;
@@ -38,18 +124,24 @@ namespace taskmaker_wpf.ViewModels {
             Console.WriteLine(text);
         }
 
-        public ObservableCollection<Motor> Motors { get; private set; }
+        public ObservableCollection<BindableMotor> Motors { get; private set; }
 
         private IRegionManager _regionManager;
         private MotorService _motorService;
+        private SerialService _serialSrv;
+
         public RegionMotorViewModel(
             IRegionManager regionManager,
+            SerialService serialSrv,
             MotorService motorService) {
             _regionManager = regionManager;
             _motorService = motorService;
+            _serialSrv = serialSrv;
 
-            Motors = new ObservableCollection<Motor>();
-            Motors.AddRange(_motorService.Motors);
+            Motors = new ObservableCollection<BindableMotor>();
+            _motorService.Motors.ForEach(e => {
+                Motors.Add(new BindableMotor(e));
+            });
         }
 
         public void OnNavigatedTo(NavigationContext navigationContext) {
--- Views/RegionMotor.xaml.cs
+++ Views/RegionMotor.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,28 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace taskmaker_wpf.Views {
+    public class MotorInfo {
+        public string BoardId { get; set; }
+        public string MotorId { get; set; }
+    }
+
+    public class MotorInfoConverter : IMultiValueConverter {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture) {
+            if (values[0] != null && values[1] != null && values.Length == 2) {
+                var boardId = values[0].ToString();
+                var motorId = values[1].ToString();
+
+                return new MotorInfo { BoardId = boardId, MotorId = motorId };
+            }
+
+            return null;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+
     /// <summary>
     /// Interaction logic for MainWindowNew.xaml
     /// </summary>
