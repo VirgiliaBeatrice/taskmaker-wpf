60f2218845a1922d9e339cc2e7eb70c9dca7ea4e
Haoyan.Li <haoyan.li.ac@gmail.com>
2022-05-11 16:54:32 +0900

    feat: RegionMotor could add/remove element

--- ViewModels/RegionMotorViewModel.cs
+++ ViewModels/RegionMotorViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
 using Prism.Regions;
 using Prism.Services.Dialogs;
 using System;
@@ -7,11 +8,35 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using taskmaker_wpf.Model.Data;
 using taskmaker_wpf.Services;
 
 namespace taskmaker_wpf.ViewModels {
     public class RegionMotorViewModel : BindableBase, INavigationAware {
+        private ICommand removeMotorCmd;
+        public ICommand RemoveMotorCmd => removeMotorCmd ?? (removeMotorCmd = new DelegateCommand<Motor>(RemoveMotorCmdExecute));
+
+        private void RemoveMotorCmdExecute(Motor obj) {
+            _motorService.Motors.Remove(obj);
+            Motors.Remove(obj);
+        }
+
+        private ICommand addMotorCmd;
+        public ICommand AddMotorCmd => addMotorCmd ?? (addMotorCmd = new DelegateCommand(AddMotorCmdExecute));
+
+        private void AddMotorCmdExecute() {
+            var newMotor = new Motor();
+            _motorService.Motors.Add(newMotor);
+            Motors.Add(newMotor);
+        }
+
+        private ICommand setCmd;
+        public ICommand SetCmd => setCmd ?? (setCmd = new DelegateCommand<string>(SetCmdExecute));
+
+        private void SetCmdExecute(string text) {
+            Console.WriteLine(text);
+        }
 
         public ObservableCollection<Motor> Motors { get; private set; }
 
@@ -36,9 +61,5 @@ namespace taskmaker_wpf.ViewModels {
         public void OnNavigatedFrom(NavigationContext navigationContext) {
             //throw new NotImplementedException();
         }
-
-        private double width;
-
-        public double Width { get => width; set => SetProperty(ref width, value); }
     }
 }
