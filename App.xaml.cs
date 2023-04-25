db00740166087aff605259f6260b220fe49eea20
Haoyan.Li <haoyan.li.ac@gmail.com>
2023-04-25 16:03:20 +0900

    misc: remove unnecessary files

--- App.xaml.cs
+++ App.xaml.cs
@@ -138,10 +138,10 @@ namespace taskmaker_wpf {
             containerRegistry.RegisterForNavigation<RegionSettings>();
             containerRegistry.RegisterForNavigation<RegionControlUI>();
             containerRegistry.RegisterForNavigation<RegionSlider>();
-            containerRegistry.RegisterForNavigation<RegionSliderPanda>();
+            //containerRegistry.RegisterForNavigation<RegionSliderPanda>();
 
             ViewModelLocationProvider.Register<RegionSlider>(() => Container.Resolve<RegionMotorViewModel>());
-            ViewModelLocationProvider.Register<RegionSliderPanda>(() => Container.Resolve<RegionMotorViewModel>());
+            //ViewModelLocationProvider.Register<RegionSliderPanda>(() => Container.Resolve<RegionMotorViewModel>());
         }
 
         protected override void Initialize() {
